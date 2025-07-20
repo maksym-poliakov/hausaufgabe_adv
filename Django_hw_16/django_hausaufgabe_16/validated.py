3fdc911df40f53df6df0e84ced538fcb383f79bc
max <max020780@gmail.com>
2025-07-20 14:09:40 +0200

    add file validated.py

--- Django_hw_16/django_hausaufgabe_16/validated.py
+++ Django_hw_16/django_hausaufgabe_16/validated.py
@@ -5,13 +5,8 @@ from django.utils import timezone
 def validated_name(value, instance=None):
     if instance and instance.name == value:
         return value
-
-    queryset = Category.objects.filter(name=value, is_deleted=False)
-    if instance:
-        queryset = queryset.exclude(instance.id)
-    if queryset.exists():
-        raise serializers.ValidationError("Category with this name already exists")
-
+    if Category.objects.filter(name=value).exists():
+        raise serializers.ValidationError('Category is exists')
     return value
 
 
